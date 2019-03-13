944d6484c2e1504a0709813f753e48f9750a501d
Florent Duval <florent3300047@gmail.com>
2019-03-13 16:10:16 +0100

    commit initial

---
+++ _head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Les 4 FantasGit</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Numans'>
+    <link rel="stylesheet" media="screen and (max-width: 640px)" href="style.css" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Pontano+Sans|Sintony" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+</head>
